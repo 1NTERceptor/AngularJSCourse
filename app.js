42dee0ecc648199b7af44fca3956f1ae7bbd8e69
Janek <gornada@gmail.com>
2025-07-13 14:06:35 +0200

    sercvices

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
-myApp.controller('mainController', function($scope) {
+myApp.controller('mainController', function($scope, $log, $filter, $resource) {
     
+    console.log($scope);
+    console.log($log);
+    console.log($filter);
+    console.log($resource);
+    
+    console.log("Hello World");
+    console.info("info");
+    console.warn("warn");
+    console.debug("debug");
+    console.error("error");
+
+    $scope.name = "John Doe";
+    $scope.formattedname = $filter('uppercase')($scope.name);
+
+    $log.info($scope.name);
+    $log.info("Formatted Name: " + $scope.formattedname);
 });
-
-var searchPeople = function(firstName, lastName, height, age, occupation) {
-    return 'Jane Doe';
-}
-
-var searchPeopleString = searchPeople.toString();
-console.log(searchPeople);
